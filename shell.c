ddd734f5012439f772ad43beca4fd9f3777c2fe7
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2020-10-31 16:27:30 -0300

    Feito fork, execvp, wait. Já é possivel executar comandos do terminal.

--- shell.c
+++ shell.c
@@ -9,8 +9,11 @@ void executarComando(char* args[]){
 
 	char* comando = args[0];
 
+	if (args[0] == NULL){
+		return;
+	}
 	if (strcmp(args[0], "quit") == 0) {
-		printf("quitando\n");
+		//printf("quitando\n");
 		exit(0);
 	}
 
@@ -19,7 +22,7 @@ void executarComando(char* args[]){
 		perror("Erro no fork");
 	}
 	if (pid == 0) { // Filho executa esse bloco
-		printf("---Filho %d\n", (int)pid);		
+		//printf("---Filho %d\n", (int)pid);		
 		if (execvp(args[0], args) == -1) {
 			perror("Erro no execv");
 		}
@@ -31,11 +34,8 @@ void executarComando(char* args[]){
 		perror("Erro no wait");
 	}
 	
-	printf("---Pai %d\n", (int)pid);
-
-
+	//printf("---Pai %d\n", (int)pid);
 }
-
 
 void splits(char *str){
 	/*
@@ -49,96 +49,55 @@ void splits(char *str){
 		token = strtok(NULL, delimVirgula);
 	}
 	*/
-	
+	char **args;
+	int indice = 0, i = 0;
     char *end_str;
     char *token = strtok_r(str, ",", &end_str);
 
     while (token != NULL) {
         char *end_token;
-        printf("comando completo = %s\n", token);
+        //printf("comando completo = '%s'\n", token);
         char *token2 = strtok_r(token, " ", &end_token);
-        while (token2 != NULL) {
-            printf("parte = %s\n", token2);
 
-            // Criar args aqui com tokens2
+        args = malloc(sizeof(char*)*512); //Aloca 512 ponteiros de char, ou seja, 512 strings **vazias**, ainda **não alocadas**.
+        while (token2 != NULL) {
+            //printf("parte = %s\n", token2);
+
+            // Criar args aqui com cada token2
+            
+  			args[indice] = malloc(sizeof(char)*512); //String Dinâmica Normal
+			args[indice] = token2;			
+			//printf("args[%d] = %s\n", indice, args[indice]);
+			indice++;
+
+            // Fim
 
             token2 = strtok_r(NULL, " ", &end_token);
         }
+        args[indice] = NULL; // No execvp, o ultimo args deve ser NULL
+        //printf("Executando um comando aq\n");
+        executarComando(args);
+        // Desalocando a memoria        
+		free(args);
+		indice = 0;
+
         token = strtok_r(NULL, ",", &end_str);
     }
 
 }
 
 int main( ){
-	/*
-  	// Cria um professo filho com pid == 0.
-	pid_t pid = fork();
-	if (pid == -1) {
-		perror("Erro no fork");
-	}
-	if (pid == 0) { // Filho executa esse bloco
-		printf("Filho %d\n", (int)pid);		
-	}
-	if (pid > 0) { // Pai, executa esse bloco  		
-  		// Espera a execução do processo filho, filho tem pid == 0.
-  		if (wait(0) == -1) {
-  			perror("Erro no wait");
-  		}
-		
-		printf("Pai %d\n", (int)pid);
-	}
-	*/
+
 	char entrada[512];
 	do{
-
-		printf("meu-shell> ");
+		printf("\e[92m");	// Muda a cor da fonte do terminal para verde
+		printf("meu-shell> ");		
+		printf("\e[0m");	// Volta a cor da fonte do terminal para branco
 		gets(entrada);
 		fflush(stdin);
-		char* args[4];
-		char* comando = entrada;
-
-		args[0] = comando;
-		args[1] = NULL;
-		args[2] = NULL;
-		args[3] = NULL;
-
 		splits(entrada);
 
-		if (strcmp(args[0], "quit") == 0) {
-			printf("quitando\n");
-			exit(0);
-		}
-
-		pid_t pid = fork();
-		if (pid == -1) {
-			perror("Erro no fork");
-		}
-		if (pid == 0) { // Filho executa esse bloco
-			printf("---Filho %d\n", (int)pid);		
-			if (execvp(args[0], args) == -1) {
-				perror("Erro no execv");
-			}
-			exit(0);
-		}
-
-		// Processo pai espera filho terminar de executar
-		if (wait(0) == -1) {
-			perror("Erro no wait");
-		}
-		
-		printf("---Pai %d\n", (int)pid);
-
-
 	}while(1);
-	
-
-
-
-
-
-	
 
 	return 0;
-
-  //exit(0);
 }
