5dc71945853ee3f35d5919f12473124b08724239
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2020-10-31 01:56:57 -0300

    Entedendo fork, wait e execv. Fazendo o tratamento da entrada.

--- shell.c
+++ shell.c
@@ -1,76 +1,107 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-/*
-void split(char *strs){
+void split(char *str){
 
-	char str[] = "strtok needs to be called several     times to split a string";
-	int init_size = strlen(str);
-	char delim[] = " ";
+	//char str[] = *strs;
 
-	char *ptr = strtok(str, delim);
 
-	while (ptr != NULL)
+	// Separa os comandos concatenados por , (virgular)
+	char delimComandos[] = ",";
+	char *ptr1 = strtok(str, delimComandos);
+
+	while (ptr1 != NULL)
 	{
-		printf("'%s'\n", ptr);
-		ptr = strtok(NULL, delim);
-	}
+		printf("'%s'\n\n", ptr1);
+		char *ptrAux = ptr1;
+		// Separa por espaços, comando + args
+		char delimPalavras[] = " ";
+		char *ptr2 = strtok(ptr1, delimPalavras);
+		while(ptr2 != NULL){
+			printf("'%s'\n", ptr2);
 
-}*/
-// Faz o split por espaços e por ;
-void split(char entrada[512]){
-
-	//char str[] = "strtok needs to be called several     times to split a string";
-	char str[512];
-	strcpy(str, entrada);
-	char delim[] = ",";
-
-	char *ptr = strtok(str, delim);
-	while (ptr != NULL) // Split por virgula
-	{
-		printf("'%s'\n", ptr);
-		printf("VIRGULA\n");
-		while(){
-
+			ptr2 = strtok(NULL, delimPalavras);
 		}
-
-
-
-
-		// Split por virgula
-		ptr = strtok(NULL, delim);
+		ptr1 = ptrAux;
+		ptr1 = strtok(NULL, delimComandos);
 	}
 
 }
 
-
-int main(){
-	char strs;
+int main( ){
+	/*
+  	// Cria um professo filho com pid == 0.
+	pid_t pid = fork();
+	if (pid == -1) {
+		perror("Erro no fork");
+	}
+	if (pid == 0) { // Filho executa esse bloco
+		printf("Filho %d\n", (int)pid);		
+	}
+	if (pid > 0) { // Pai, executa esse bloco  		
+  		// Espera a execução do processo filho, filho tem pid == 0.
+  		if (wait(0) == -1) {
+  			perror("Erro no wait");
+  		}
+		
+		printf("Pai %d\n", (int)pid);
+	}
+	*/
 	char entrada[512];
-	
-
-	int i = 0;
-
 	do{
-		printf("meu-shell>");
+
+		printf("meu-shell> ");
 		gets(entrada);
 		fflush(stdin);
-		printf("%s\n", entrada);
-		i++;
-	}while(i<3);
+		char* args[4];
+		char* comando = entrada;
 
+		args[0] = comando;
+		args[1] = NULL;
+		args[2] = NULL;
+		args[3] = NULL;
+
+		split(entrada);
+
+		if (strcmp(args[0], "quit") == 0) {
+			printf("quitando\n");
+			exit(0);
+		}
+
+		pid_t pid = fork();
+		if (pid == -1) {
+			perror("Erro no fork");
+		}
+		if (pid == 0) { // Filho executa esse bloco
+			printf("---Filho %d\n", (int)pid);		
+			if (execvp(args[0], args) == -1) {
+				perror("Erro no execv");
+			}
+			exit(0);
+		}
+
+		// Processo pai espera filho terminar de executar
+		if (wait(0) == -1) {
+			perror("Erro no wait");
+		}
+		
+		printf("---Pai %d\n", (int)pid);
+
+
+	}while(1);
 	
 
 
+		
+
+
 	
-	split(entrada);
+
 	return 0;
 
+  //exit(0);
 }
-
-
-
-
-
-
