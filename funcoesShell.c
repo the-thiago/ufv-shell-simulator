0535109b384f5f7896dae91c0931af1dc3ef7fcc
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2020-11-05 20:09:00 -0300

    Programa agora simula o pipe'|' do terminal

--- funcoesShell.c
+++ funcoesShell.c
@@ -7,67 +7,110 @@
 #include <errno.h>
 #include "funcoesShell.h"
 
-void executarComando(char* args[]){
+void executarComando(char* args[], char* proximoComando, int primeiroComandoOuNao, int* fd){
 
 	if (args[0] == NULL){ // Não digitou nada alem de espaços e virgulas.
 		return;
 	}
-	if (strcmp(args[0], "quit") == 0) {
-		//printf("quitando\n");
+	if (strcmp(args[0], "quit") == 0) { // Fecha programa
 		exit(0);
 	}
 
-	pid_t pid = fork(); // Cria um processo filho
-	if (pid == -1) {
-		perror("Erro no fork");
-	}
-	if (pid == 0) { // Filho executa esse bloco
-		//printf("---Filho %d\n", (int)pid);		
-		if (execvp(args[0], args) == -1) {
-			perror("Erro ao executar comando"); // Printa errno
+	// É o primeiro comando e tem mais comandos concatenados por ','
+	if(primeiroComandoOuNao == 1 && proximoComando != NULL){ 
+		pid_t pid = fork(); // Cria um processo filho
+		if (pid == -1) {
+			perror("Erro no fork");
 		}
-		exit(0); // Termina processo filho
+		if (pid == 0) { // Filho executa esse bloco		
+			dup2(fd[1], STDOUT_FILENO);
+			close(fd[0]);			
+			close(fd[1]);
+
+			if (execvp(args[0], args) == -1) {
+				perror("Erro ao executar comando"); // Printa errno
+			}
+		}
+
+	} else if(primeiroComandoOuNao == 0 && proximoComando == NULL){ // Não é o primeiro e é o ultimo comando
+
+		pid_t pid = fork(); // Cria um processo filho
+		if (pid == -1) {
+			perror("Erro no fork");
+		}
+		if (pid == 0) { // Filho executa esse bloco	
+			dup2(fd[0], STDIN_FILENO);	
+			close(fd[0]);
+			close(fd[1]);
+
+			if (execvp(args[0], args) == -1) {
+				perror("Erro ao executar comando"); // Printa errno
+			}
+		}
+		// Fecha pipe do programa principal
+		close(fd[0]);
+		close(fd[1]);
+
+	} else if(primeiroComandoOuNao == 1 && proximoComando == NULL){ // Um unico comando, primeiro e ultimo
+
+		pid_t pid = fork(); // Cria um processo filho
+		if (pid == -1) {
+			perror("Erro no fork");
+		}
+		if (pid == 0) { // Filho executa esse bloco	
+			if (execvp(args[0], args) == -1) {
+				perror("Erro ao executar comando"); // Printa errno
+			}
+		}		
 	}
 
 	// Processo pai espera filho terminar de executar
 	if (wait(0) == -1) {
 		perror("Erro no wait");
 	}	
-	//printf("---Pai %d\n", (int)pid);
 }
 
 void tratarEntrada(char *str){
-
+	int primeiroComandoOuNao = 1;
+	int fd[2];		// files descriptors, faz a comunicação entre processos
 	char **args;
-	int indice = 0; // Ultimo args adicionado ao vetor de strings(char **args)
-    char *end_str;
-    char *token = strtok_r(str, ",", &end_str);
+	int indice = 0; // Indice do ultimo args adicionado ao vetor de strings(char **args)
+	char *end_str;
 
-    while (token != NULL) { // Split por virgula ','
-    	//printf("comando completo: '%s'\n", token);
-        char *end_token;        
-        char *token2 = strtok_r(token, " ", &end_token);
+	char *token = strtok_r(str, ",", &end_str);
 
-        // Vetor de Strings criado
-        args = malloc(sizeof(char*)*TAMANHO_ENTRADA); //Aloca 512 ponteiros de char, ou seja, 512 strings **vazias**, ainda **não alocadas**.
-        while (token2 != NULL) { // Split por espaço ' '
-            //printf("pedaço do comando: %s\n", token2);
+	if (pipe(fd) == -1) {
+		perror("Erro no pipe");
+		return ;
+	}
 
-            // Criar args aqui com cada token2(Pedaço do comando que estão sendo separados por ' '),          
-  			args[indice] = malloc(sizeof(char)*TAMANHO_ENTRADA);
+	while (token != NULL) { // Split por virgula ','
+		//printf("comando completo: '%s'\n", token);
+		char *end_token;        
+		char *token2 = strtok_r(token, " ", &end_token);
+
+		// Vetor de Strings criado
+		args = malloc(sizeof(char*)*TAMANHO_ENTRADA); //Aloca 512 ponteiros de char, ou seja, 512 strings **vazias**, ainda **não alocadas**.
+		while (token2 != NULL) { // Split por espaço ' '
+			//printf("pedaço do comando: %s\n", token2);
+
+			// Criar args aqui com cada token2(Pedaço do comando que estão sendo separados por ' '),          
+			args[indice] = malloc(sizeof(char)*TAMANHO_ENTRADA);
 			args[indice] = token2;			
-			//printf("args[%d] = %s\n", indice, args[indice]);
 			indice++;
 
-            token2 = strtok_r(NULL, " ", &end_token);
-        }
-        args[indice] = NULL; // No execvp, o ultimo args deve ser NULL
-        //printf("Executando um comando aq\n");
-        executarComando(args);
-        // Desalocando a memoria        
+			token2 = strtok_r(NULL, " ", &end_token);
+		}
+		args[indice] = NULL; // No execvp, o ultimo args deve ser NULL       
+
+		token = strtok_r(NULL, ",", &end_str); // Pega proximo comando completo...
+		// Executa o comando que está no vetor de string args
+		executarComando(args, token, primeiroComandoOuNao, fd);
+		// Desalocando a memoria        
 		free(args);
 		indice = 0;
-
-        token = strtok_r(NULL, ",", &end_str);
-    }
+		if (primeiroComandoOuNao == 1){
+			primeiroComandoOuNao = 0;
+		}
+	}
 }
--- funcoesShell.h
+++ funcoesShell.h
@@ -2,7 +2,7 @@
 #define FUNCOES_SHELL
 
 #define TAMANHO_ENTRADA 512
-void executarComando(char* args[]);
+void executarComando(char* args[], char* proximoComando, int primeiroComandoOuNao, int* fd);
 void tratarEntrada(char *str);
 
 #endif
